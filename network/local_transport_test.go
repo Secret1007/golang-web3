f55cda1c9e1d1d65265d7ab53e22b90fd1c63cc1
root <root@SECRET-SUN>
2024-05-13 09:45:33 +0800

    添加测试

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -16,3 +16,18 @@ func TestConnect(t *testing.T) {
 	assert.Equal(t, tra.peers[trb.addr], trb)
 	assert.Equal(t, trb.peers[tra.addr], tra)
 }
+
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	msg := []byte("hello world")
+	assert.Nil(t, tra.SendMessage(tra.addr, msg))
+
+	rpc := <-trb.Consume()
+	assert.Equal(t, rpc.Payload, msg)
+	assert.Equal(t, rpc.From, tra.addr)
+}
